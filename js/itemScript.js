0bc1acd1667d5280719b799f74992f85e3bcddc3
magdalenazucal <magdalenazucal@gmail.com>
2023-05-12 14:42:34 -0300

    arreglos

--- js/itemScript.js
+++ js/itemScript.js
@@ -1,7 +1,3 @@
-
-//Aca lo primero que hacés es encontrar el boton que vas a necesitar apretar para que pase algo. En este caso lo obtenes a traves del query selector de clase .input-plus; le agregas el escuchador de eventos y le indicas que el evento es click y que lo que tiene que hacer es sumar, y declaras abajo qué hace la funcion sumar. Lo mismo con restar. 
-//Asi podes sacar el "onclick" del html.
-
 document.querySelector(".input-plus").addEventListener('click', sumar);
 
 function sumar() { 	
@@ -23,7 +19,6 @@ function restar(){
     
     if(num == '' || num == 0){
         num ="0";
-        alert("No tiene items para quitar"); 
     }
    
     if(num > "0"){
@@ -31,11 +26,3 @@ function restar(){
         document.getElementById('num').value = restar;
     }
 }
-
-// let num = document.querySelector("#num");
-
-// num.value.addEventListener('change', ()=>{
-// 	if(num.value > 9){
-// 		num.style
-// 	}
-// })
